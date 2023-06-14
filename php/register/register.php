707d3a9d0dd9a55b0b294f250ca7b9579345dfcc
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-06-14 20:48:52 +0300

    Add registration

---
+++ php/register/register.php
@@ -0,0 +1,28 @@
+<?php
+
+$phpInput = json_decode(file_get_contents('php://input'), true);
+// $phpInput = $_POST;
+
+// validate php input
+
+$username = $phpInput['username'];
+$first_name = $phpInput['first_name'];
+$last_name = $phpInput['last_name'];
+$email = $phpInput['email'];
+$password = $phpInput['password'];
+$deleted = $phpInput['deleted'];
+
+require_once "../user/User.php";
+
+$user = new User($username, $first_name, $last_name, $email, $password, $deleted);
+
+try {
+    $user->saveNewUser();
+    
+    echo json_encode(['success' => true]);
+} catch (Exception $e) {
+    echo json_encode([
+        'success' => false,
+        'message' => $e->getMessage(),
+    ]);
+}
---
+++ php/user/UserValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class UserValidator {
+    public function validateUsername($username): void {
+        $this->validateStringValue($username, 'username');
+    }
+
+    public function validateFirstName($first_name): void {
+        $this->validateStringValue($first_name, 'first name');
+    }
+
+    public function validateLastName($last_name): void {
+        $this->validateStringValue($last_name, 'last name');
+    }
+
+    public function validateEmail($email): void {
+        $this->validateStringValue($email, 'email');
+    }
+
+    public function validatePassword($password): void {
+        $this->validateStringValue($password, 'password');
+    }
+
+    public function validateStringValue($value, $text) : void {
+        if ($value == null || empty($value)) {
+            throw new InvalidArgumentException('The constructor accepts only string values for '.$text.'!');
+        }
+    }
+}
