2bd8096ef073ba2e9dc740d4b0b56a01616edd69
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-06-17 20:16:11 +0300

    Add playing chords functionality

--- scripts/chords.js
+++ scripts/chords.js
@@ -26,10 +26,49 @@ const showAllChordsData = () => {
             chordInfoElement.setAttribute("id", "div"  + chord[i].id);
             chordInfoElement.innerHTML = `<span> Name: ${chord[i].name} </span>
                                         <span> Description: ${chord[i].description} </span>`;
+                            
+            const chordPlayButton = document.createElement('button');
+            chordPlayButton.setAttribute("id", "button"  + chord[i].id);
+            chordPlayButton.innerHTML = 'Чуй акорда!';
+            
+            
 
             container.appendChild(chordInfoElement);
+            container.appendChild(chordPlayButton);
+
+            playChords(chord[i]);
             }
         });
+}
+
+function playChords(chord) {
+    
+    document.getElementById("button"  + chord.id).addEventListener('click', () => {
+        chord_notes = chord.description.split("-");
+        console.log(chord_notes);
+
+        var snd1  = new Audio();
+        var src1  = document.createElement("source");
+        src1.type = "audio/mpeg";
+        src1.src  = '../assets/' + chord_notes[0] + '.mp3';
+        snd1.appendChild(src1);
+        
+        var snd2  = new Audio();
+        var src2  = document.createElement("source");
+        src2.type = "audio/mpeg";
+        src2.src  = '../assets/' + chord_notes[1] + '.mp3';
+        snd2.appendChild(src2);
+
+        var snd3  = new Audio();
+        var src3  = document.createElement("source");
+        src3.type = "audio/mpeg";
+        src3.src  = '../assets/' + chord_notes[2] + '.mp3';
+        snd3.appendChild(src3);
+        
+        snd1.play(); 
+        snd2.play(); 
+        snd3.play();
+    });  
 }
 
 showAllChordsData();
