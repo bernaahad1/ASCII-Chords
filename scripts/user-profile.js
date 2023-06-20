567d618c56ca49af699f14d280ee2f77edf60848
Berna Ahad <bernaahad1@gmail.com>
2023-06-20 21:25:57 +0300

    Edit user js

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -133,23 +133,23 @@ class UserProfile extends HTMLElement {
 
   fetchUserInformation() {
     // TODO Replace this with fetch users
-    const fakeFetch = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          firstName: "John",
-          lastName: "Doe",
-          email: "johndoe@example.com",
-          username: "johndoe"
-        });
-      }, 2000);
-    });
-
-    fakeFetch.then((user) => {
-      this.user = user;
-      this.removeInitialLoader();
-      this.renderUser();
-      this.setupEditButton();
-    });
+    fetch("../php/user/UserEndpoints.php", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(user);
+        this.user = user;
+        this.removeInitialLoader();
+        this.renderUser();
+        this.setupEditButton();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   removeInitialLoader() {
@@ -181,25 +181,25 @@ class UserProfile extends HTMLElement {
 
     loader.classList.remove("hidden");
 
-    // TODO Replace this with edit user method fetch
-    const fakeFetch = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const errorOccurred = Math.random() < 0.5; // Simulate an error with a 50% chance
+    const editedInfo = {
+      username: updatedUsername,
+      first_name: updatedFirstName,
+      last_name: updatedLastName
+    };
 
-        if (errorOccurred) {
-          reject("Error occurred while saving user information.");
-        } else {
-          resolve();
-        }
-      }, 2000);
-    });
-
-    fakeFetch
-      .then(() => {
-        this.user.firstName = updatedFirstName;
-        this.user.lastName = updatedLastName;
-        this.user.email = updatedEmail;
-        this.user.username = updatedUsername;
+    fetch("../php/user/UserEndpoints.php", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedInfo)
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        this.user = user;
+        // this.user.lastName = updatedLastName;
+        // this.user.email = updatedEmail;
+        // this.user.username = updatedUsername;
 
         firstNameInput.disabled = true;
         lastNameInput.disabled = true;
