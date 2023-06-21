96dd40a2b5574fdf2aa149d87b1157e4ab52a57d
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-06-21 14:01:53 +0300

    Add txt import

--- php/import/ImportEndpoints.php
+++ php/import/ImportEndpoints.php
@@ -6,21 +6,13 @@ $response = null;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['csv_file_path'])) {
     $response = ImportRequestHandler::getAllDataFromCSV($_GET['csv_file_path']);
-    //$response = "get here";
 }
-
-// elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['chord_id'])) {
-//     $response = FavouriteChordsRequestHandler::getAllRecordsByChordId($_GET['chord_id']);
-// }
-// elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['user_id']) && isset($_GET['chord_id'])) {
-//     $response = FavouriteChordsRequestHandler::getFavouriteChordByUserIdAndChordId($_GET['user_id'], $_GET['chord_id']);
-// }
-// elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['user_id']) && isset($_GET['chord_id'])) {
-//     $response = FavouriteChordsRequestHandler::addFavouriteChord($_GET['user_id'], $_GET['chord_id']);
-// }
-// elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE' && isset($_GET['user_id']) && isset($_GET['chord_id'])) {
-//     $response = FavouriteChordsRequestHandler::deleteFavouriteChord($_GET['user_id'], $_GET['chord_id']);
-// }
+elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['txt_file_path'])) {
+    $response = ImportRequestHandler::getAllDataFromTXT($_GET['txt_file_path']);
+}
+elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['json_file_path'])) {
+    // $response = ImportRequestHandler::getAllDataFromJSON($_GET['json_file_path']);
+}
 
 if ($response == null) {
     $response = "There is no data to display!";
--- php/import/ImportRequestHandler.php
+++ php/import/ImportRequestHandler.php
@@ -7,23 +7,83 @@ class ImportRequestHandler extends ImportRequestHandlerValidator {
     public static function getAllDataFromCSV($filePath) {
         self::validateFilePath($filePath);
 
-        $csv = array_map('str_getcsv', file($filePath));
-        self::validateFileData($csv);
-
         try {
-            for ($i = 0; $i < count($csv); $i++) {
-                ChordRequestHandler::addNewChord($csv[$i][0], $csv[$i][1]);
-            }
+            self::loadDataFromCsv($filePath);
             
-            // return true;
-            echo json_encode(['success' => true]);
+            return ['success' => true];
         } catch (Exception $e) {
-            // return false;
-            echo json_encode([
+            return [
                  'success' => false,
                  'message' => $e->getMessage(),
-            ]);
+            ];
         }
+    }
 
+    public static function getAllDataFromTXT($filePath) {
+        self::validateFilePath($filePath);
+
+        try {
+            self::loadDataFromTxt($filePath);
+            
+            return ['success' => true];
+        } catch (Exception $e) {
+            return [
+                 'success' => false,
+                 'message' => $e->getMessage(),
+            ];
+        }
+    }
+
+    public static function getAllDataFromJSON($filePath) {
+        self::validateFilePath($filePath);
+
+        try {
+            self::loadDataFromJson($filePath);
+            
+            return ['success' => true];
+        } catch (Exception $e) {
+            return [
+                 'success' => false,
+                 'message' => $e->getMessage(),
+            ];
+        }
+    }
+
+    private static function loadDataFromCsv($filePath) {
+        $csv = array_map('str_getcsv', file($filePath));
+            self::validateFileData($csv);
+
+            for ($i = 0; $i < count($csv); $i++) {
+                self::saveChord($csv[$i][0], $csv[$i][1]);
+            }
+    }
+
+    private static function loadDataFromTxt($filePath) : void {
+        $handle = fopen($filePath, "r");
+        if ($handle) {
+            $i=0;
+            while (($line = fgets($handle)) !== false) {
+                $data = self::prepareData($line);
+                self::saveChord($data[0], $data[1]);
+
+                $i++;
+            }
+            fclose($handle);
+        } else {
+            throw new RuntimeException("There was problem in opening the file!");
+        }
+    }
+
+    private static function loadDataFromJson($filePath) {
+        
+    }
+
+    private static function prepareData($line) : array {
+        $line = trim($line);
+        return explode('|', $line);
+    }
+
+    private static function saveChord($name, $description) : void {
+        ChordRequestHandler::addNewChord($name, $description);
     }
 }
